37be7b6d31804e87ee27cb573cac9e8ebe61e1bc
Abdul Uddin <auddin431@MacBook-Pro.fios-router.home>
2021-03-19 12:19:49 -0400

    commented on code

--- flight/src/components/Info.js
+++ flight/src/components/Info.js
@@ -4,7 +4,32 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
+const placeFinder = (arr, id) => {
+  // Linear search to find a place given the PlaceId
+  let n = arr.length;
+  let i = 0;
+  for (i = 0; i < n; i++) {
+    if (arr[i].PlaceId === id) {
+      return i;
+    }
+  }
+  return null;
+};
+
+const carrierFinder = (arr, id) => {
+  // Linear search to find a carrier given the CarrierId
+  let n = arr.length;
+  let i = 0;
+  for (i = 0; i < n; i++) {
+    if (arr[i].CarrierId === id) {
+      return i;
+    }
+  }
+  return null;
+};
+
 const OutlinedCard = (props) => {
+  // Component to store route information to be displayed
   return (
     <div>
       <Card className="root" variant="outlined">
@@ -40,29 +65,8 @@ const OutlinedCard = (props) => {
   );
 };
 
-const placeFinder = (arr, id) => {
-  let n = arr.length;
-  let i = 0;
-  for (i = 0; i < n; i++) {
-    if (arr[i].PlaceId === id) {
-      return i;
-    }
-  }
-  return null;
-};
-
-const carrierFinder = (arr, id) => {
-  let n = arr.length;
-  let i = 0;
-  for (i = 0; i < n; i++) {
-    if (arr[i].CarrierId === id) {
-      return i;
-    }
-  }
-  return null;
-};
-
 const InfoWrapper = (props) => {
+  // Component to wrap route information and extract required information from API response
   return (
     <>
       {props.areRoutes ? (
@@ -70,16 +74,16 @@ const InfoWrapper = (props) => {
           let departureOutboundDate = quote.OutboundLeg.DepartureDate.slice(
             0,
             10
-          );
+          ); // Extracting "yyyy-mm-dd" string from the outbound leg
           let originId = quote.OutboundLeg.OriginId;
           let carrierOutboundId = quote.OutboundLeg.CarrierIds[0];
           let destinationId = quote.OutboundLeg.DestinationId;
-          let originIndex = placeFinder(props.route.Places, originId);
-          let destinationIndex = placeFinder(props.route.Places, destinationId);
+          let originIndex = placeFinder(props.route.Places, originId); // Linear search to find where the origin is within Places array of response
+          let destinationIndex = placeFinder(props.route.Places, destinationId); // Finds destination within Places
           let carrierOutboundIndex = carrierFinder(
             props.route.Carriers,
             carrierOutboundId
-          );
+          ); // Finds outbound carrier location
           let carrierOutbound = props.route.Carriers[carrierOutboundIndex].Name;
           let carrierInboundId;
           let carrierInboundIndex;
@@ -89,9 +93,9 @@ const InfoWrapper = (props) => {
           let destinationName;
 
           if (props.route.Places[originIndex].Type !== "Station") {
-            originName = props.route.Places[originIndex].Name;
+            originName = props.route.Places[originIndex].Name; // If the location is not a station, extract name to be displayed
           } else {
-            originName = `${props.route.Places[originIndex].CityName} (${props.route.Places[originIndex].IataCode})`;
+            originName = `${props.route.Places[originIndex].CityName} (${props.route.Places[originIndex].IataCode})`; // If it is a station, use city name and iata code to describe instead
           }
           if (props.route.Places[destinationIndex].Type !== "Station") {
             destinationName = props.route.Places[destinationIndex].Name;
@@ -99,6 +103,7 @@ const InfoWrapper = (props) => {
             destinationName = `${props.route.Places[destinationIndex].CityName} (${props.route.Places[destinationIndex].IataCode})`;
           }
           if (quote.hasOwnProperty("InboundLeg")) {
+            // Extract inbound information if it exists
             departureInboundDate = quote.InboundLeg.DepartureDate.slice(0, 10);
             carrierInboundId = quote.InboundLeg.CarrierIds[0];
           } else {
@@ -144,6 +149,9 @@ const InfoWrapper = (props) => {
 const Info = (props) => {
   let copyResponse = { ...props.route };
   if (props.sortOption.value === "highest") {
+    // Implementation of the sort functionality
+    // If sort from highest to lowest is selected, the quotes will be in reverse order since the API
+    // response is already sorted from lowest to highest
     copyResponse.Quotes = props.route.Quotes.slice(0).reverse();
   }
   return <InfoWrapper areRoutes={props.areRoutes} route={copyResponse} />;
--- flight/src/components/Main.js
+++ flight/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Info from "./Info";
-import Select from "react-select";
+import Select from "react-select"; // Component for dropdown
 import DatePicker from "react-date-picker";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
@@ -9,6 +9,7 @@ import { withStyles } from "@material-ui/core/styles";
 import "./Main.css";
 
 const reqOptions = {
+  // Request options to use for each API request
   method: "GET",
   headers: {
     "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`,
@@ -17,7 +18,23 @@ const reqOptions = {
   },
 };
 
+const dateToString = (date) => {
+  // Function to convert date objects into yyyy-mm-dd string format
+  let yyyy = date.getFullYear();
+  let mm = date.getMonth() + 1;
+  let dd = date.getDate();
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+  let fullDate = `${yyyy}-${mm}-${dd}`;
+  return fullDate;
+};
+
 const BlueSwitch = withStyles({
+  // Style MaterialUI Switch Component
   switchBase: {
     color: "white",
     "&$checked": {
@@ -32,6 +49,7 @@ const BlueSwitch = withStyles({
 })(Switch);
 
 const BlueButton = withStyles((theme) => ({
+  // Style MaterialUI Button Component
   root: {
     color: "white",
     backgroundColor: "#013d5b",
@@ -42,36 +60,24 @@ const BlueButton = withStyles((theme) => ({
 }))(Button);
 
 const Main = () => {
-  const dateToString = (date) => {
-    let yyyy = date.getFullYear();
-    let mm = date.getMonth() + 1;
-    let dd = date.getDate();
-    if (mm < 10) {
-      mm = "0" + mm;
-    }
-    if (dd < 10) {
-      dd = "0" + dd;
-    }
-    let fullDate = `${yyyy}-${mm}-${dd}`;
-    return fullDate;
-  };
-  const [origin, setOrigin] = useState([]);
-  const [destination, setDestination] = useState([]);
-  const [airportOrigin, setAirportOrigin] = useState("");
-  const [airportDestination, setAirportDestination] = useState("");
-  const [currency, setCurrency] = useState("USD");
-  const [currencies, setCurrencies] = useState([]);
-  const [showRoutes, setShowRoutes] = useState(false);
-  const [originDate, setOriginDate] = useState(new Date());
-  const [obpDate, setObpDate] = useState(dateToString(new Date()));
-  const [ibpDate, setIbpDate] = useState("");
-  const [slider, setSlider] = useState(false);
-  const [destDate, setDestDate] = useState(new Date());
-  const [routeResponse, setRouteResponse] = useState({});
-  const [areRoutes, setAreRoutes] = useState(true);
-  const [sortOption, setSortOption] = useState("lowest");
+  const [currencies, setCurrencies] = useState([]); // Store options for currency dropdown
+  const [currency, setCurrency] = useState("USD"); // Defaulting currency to USD in case user forgets to select
+  const [origin, setOrigin] = useState([]); // Store options for origin dropdown
+  const [destination, setDestination] = useState([]); // Store options for destination dropdown
+  const [airportOrigin, setAirportOrigin] = useState(""); // Store airport chosen from origin dropdown
+  const [airportDestination, setAirportDestination] = useState(""); // Store airport chosen from destination dropdown
+  const [showRoutes, setShowRoutes] = useState(false); // Boolean to determine if routes are to be displayed
+  const [originDate, setOriginDate] = useState(new Date()); // Default option for origin datepicker calendar
+  const [destDate, setDestDate] = useState(new Date()); // Default option for destination datepicker calendar
+  const [obpDate, setObpDate] = useState(dateToString(new Date())); // Set default outbound partial date to today's date
+  const [ibpDate, setIbpDate] = useState(""); // Inbound partial date defaulted to empty string
+  const [slider, setSlider] = useState(false); // Slider to choose if round trip or one way
+  const [routeResponse, setRouteResponse] = useState({}); // Store API response for finding routes
+  const [areRoutes, setAreRoutes] = useState(true); // Boolean to determine if routes exist
+  const [sortOption, setSortOption] = useState("lowest"); // Defaulting to sort by lowest price
 
   useEffect(() => {
+    // Send one time API request to fetch all currencies
     const fetchCurrencies = async () => {
       try {
         let response = await fetch(
@@ -92,9 +98,11 @@ const Main = () => {
   }, []);
 
   const handleChangeOrigin = (inputValue) => {
+    // Handler to find origin airports based on user input in dropdown
     const fetchOrigin = async () => {
       try {
         if (inputValue !== "") {
+          // API Request made only if user inputted value
           let response = await fetch(
             `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/${currency}/en-US/?` +
               new URLSearchParams({ query: inputValue }),
@@ -102,6 +110,7 @@ const Main = () => {
           );
           response = await response.json();
           const newArray = response.Places.map((item) => {
+            // Extracting Airport ID and Place Name to use for dropdown
             return { value: item.PlaceId, label: item.PlaceName };
           });
           setOrigin(newArray);
@@ -113,6 +122,7 @@ const Main = () => {
     fetchOrigin();
   };
   const handleChangeDestination = (inputValue) => {
+    // Handler to find destination airports based on user input in dropdown
     const fetchDestination = async () => {
       try {
         if (inputValue !== "") {
@@ -135,6 +145,7 @@ const Main = () => {
   };
 
   const handleOnClick = () => {
+    // Handler for "Find Flight" Button
     const fetchRoute = async () => {
       try {
         let response = await fetch(
@@ -142,14 +153,15 @@ const Main = () => {
           reqOptions
         );
         response = await response.json();
-        //console.log(response);
         if (response.message || response.errors) {
-          //console.log("Error was received");
+          // If the API response has a message field, there was an internal server error.
+          // If it has an errors field, then something went wrong with the API request itself, so an error is thrown
           throw new Error("No Proper Response");
         }
         setRouteResponse(response);
         setShowRoutes(true);
         if (response.Quotes.length < 1) {
+          // If the response returns successfully but there are no quotes, it means no routes have been found
           setAreRoutes(false);
         } else {
           setAreRoutes(true);
@@ -164,6 +176,7 @@ const Main = () => {
   };
 
   const handleOriginDateChange = (returnValue) => {
+    // Handler for when user selects a particular date in the calendar
     setOriginDate(returnValue);
     if (returnValue !== null) {
       let fullDate = dateToString(returnValue);
@@ -184,8 +197,10 @@ const Main = () => {
   };
 
   const handleSlider = () => {
+    // Toggle for slider
     setSlider(!slider);
     if (!slider) {
+      // If the slider is set to false, it means one way, so no inbound partial date
       setIbpDate("");
     }
   };
